74c979534cf5ab2ded13db82859ca7b6e6c3f883
wooseungw <woow070840@gmail.com>
2023-11-16 00:30:45 +0900

    1233

---
+++ gpt4.py
@@ -0,0 +1,35 @@
+from IPython.display import display, Image, Audio
+
+import cv2  # We're using OpenCV to read video, to install !pip install opencv-python
+import base64
+import time
+from openai import OpenAI
+import os
+import requests
+from key import api_key
+client = OpenAI()
+
+# OpenAI API Key
+api_key = api_key()
+
+
+response = client.chat.completions.create(
+  model="gpt-4-vision-preview",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What’s in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0])
---
+++ key.py
@@ -0,0 +1,6 @@
+# OpenAI API Key
+
+
+def api_key():
+    api_key = "YOUR_OPENAI_API_KEY"
+    return api_key
