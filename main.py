793c1086326d8d9240f0d264ed880e1ec3eda58f
Jonghun Shin <ozingmw@naver.com>
2023-11-21 21:25:58 +0900

    gpt인자 맞춰서 수정

--- main.py
+++ main.py
@@ -16,11 +16,16 @@ def index():
             gpt4_instance = Gpt4(key = key)
             
         image = request.files['image']  # 이미지 받기
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)  # 이미지를 저장할 경로
-        image.save(filename)  # 이미지 저장
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)  # 이미지를 저장할 경로
+        image.save(filepath)  # 이미지 저장
+
         sentence = request.form['sentence']  # 문장 받기
-        result_image, result_sentence = gpt4_instance.parse(filename, sentence)  # 이미지 파일 경로와 문장을 gpt4로 전달
-        return render_template('result.html', image=result_image, sentence=result_sentence)  # 결과 표시
+        # result_image, result_sentence = gpt4_instance.parse(filepath, sentence)  # 이미지 파일 경로와 문장을 gpt4로 전달
+        result_sentence = gpt4_instance.parse(filepath, sentence)  # 현재 문장만 생성하기 때문에 출력값 하나로 축소
+
+        # return render_template('result.html', image=result_image, sentence=result_sentence)  # 결과 표시
+        return render_template('result.html', sentence=result_sentence)  # 현재 문장만 생성하기 때문에 출력값 하나로 축소
+    
     return render_template('index.html')  # 초기 페이지 표시
 
 if __name__ == "__main__":
