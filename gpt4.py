90dc93e063f1c9ccb8c47dbc6b07bbba029872c8
wooseungw <woow070840@gmail.com>
2023-11-21 20:45:17 +0900

    1233

--- gpt4.py
+++ gpt4.py
@@ -8,44 +8,47 @@ import os
 import requests
 
 
-# 파일 열기
-with open('key.txt', 'r') as file:
-    # 첫 번째 줄 읽기
-    key = file.readline()
+class Gpt4:
+  def __init__(self, key) -> None:
+    self.key = key
+    self.client = openai.OpenAI(api_key = key)
+
+  def parse(self, img, sentence):
+    self.img = img
+    self.sentence = sentence
+    response = self.client.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=[
+      {
+        "role": "system",
+        "content": [
+          {"type": "text", "text": "역활 놀이를 하자 나는 요청자고 너는 Setimental한 Photo Retouching으로 상을 받은 사진가야. \
+            나는 내가 원하는 색감이나 감성을 이미지와 함께 입력할꺼야.\
+            너는 그 이미지의 어떤 오브젝트가 있는지와 광량, 추정된 위치 등을 바탕으로, 피사체를 강조하는게 좋을지 아니면 전체적인 분위기를 보여주는게 좋을지 말해주고,그런데 [text]로 시작해서[/text]로 끝내줘.\
+            내가 보내준 사진에 어울리는 사진보정방법을 알려줘야해.\
+            사진 보정법을 알려줄땐 [retorch]로시작해서 [/retorch]로 끝내야해 그리고 안에 사진 보정 속성 (채도,하이라이트,등 속성) 중 바꿔야 할 부분을 수치화 해서 알려줘.\
+              예시로 [retorch]\n 채도 '10' , 하이라이트 '-20',... [/retorch]\
+            다 알려주면 대화 종료야"},
+        ],
+      },
+      {
+        "role": "user",
+        "content": [
+          {"type": "text", "text": "사이버 펑크느낌을 주고싶어"},
+          {
+            "type": "image_url",
+            "image_url": {
+              "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+              },
+            },
+          ],
+        }
+      ],
+      max_tokens=500,
+    )
+    return response.choices[0]
+
     
 
-client = openai.OpenAI(api_key = key)
 
 
-response = client.chat.completions.create(
-  model="gpt-4-vision-preview",
-  messages=[
-    {
-      "role": "system",
-      "content": [
-        {"type": "text", "text": "역활 놀이를 하자 나는 요청자고 너는 Setimental한 Photo Retouching으로 상을 받은 사진가야. \
-          나는 내가 원하는 색감이나 감성을 이미지와 함께 입력할꺼야.\
-          너는 그 이미지의 어떤 오브젝트가 있는지와 광량, 추정된 위치 등을 바탕으로, 피사체를 강조하는게 좋을지 아니면 전체적인 분위기를 보여주는게 좋을지 말해주고,그런데 [text]로 시작해서[/text]로 끝내줘.\
-          내가 보내준 사진에 어울리는 사진보정방법을 알려줘야해.\
-          사진 보정법을 알려줄땐 [retorch]로시작해서 [/retorch]로 끝내야해 그리고 안에 사진 보정 속성 (채도,하이라이트,등 속성) 중 바꿔야 할 부분을 수치화 해서 알려줘.\
-            예시로 [retorch]\n 채도 '10' , 하이라이트 '-20',... [/retorch]\
-          다 알려주면 대화 종료야"},
-      ],
-    },
-    {
-      "role": "user",
-      "content": [
-        {"type": "text", "text": "사이버 펑크느낌을 주고싶어"},
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
-          },
-        },
-      ],
-    }
-  ],
-  max_tokens=500,
-)
-
-print(response.choices[0])
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from flask import Flask, request, render_template
-import gpt4
+from gpt4 import Gpt4
+import os
 
 app = Flask(__name__)
+UPLOAD_FOLDER = './gpt4_picturecorrection/img_dir'  # 이미지를 저장할 위치
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # 파일 열기
+        with open('key.txt', 'r') as file:
+            # 첫 번째 줄 읽기
+            key = file.readline()
+            gpt4_instance = Gpt4(key = key)
+            
         image = request.files['image']  # 이미지 받기
+        filename = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)  # 이미지를 저장할 경로
+        image.save(filename)  # 이미지 저장
         sentence = request.form['sentence']  # 문장 받기
-        result_image, result_sentence = gpt4.parse(image, sentence)  # pgt4를 사용해서 이미지와 문장 처리
+        result_image, result_sentence = gpt4_instance.parse(filename, sentence)  # 이미지 파일 경로와 문장을 gpt4로 전달
         return render_template('result.html', image=result_image, sentence=result_sentence)  # 결과 표시
     return render_template('index.html')  # 초기 페이지 표시
 
