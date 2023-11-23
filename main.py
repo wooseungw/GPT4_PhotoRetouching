6f5f5dcf72da97140580d3ba118f14c51d2bd938
Jonghun Shin <ozingmw@naver.com>
2023-11-23 14:23:02 +0900

    result 이미지 표시추가

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, render_template
 from gpt4 import Gpt4
 import os
+import shutil
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'img_dir'  # 이미지를 저장할 위치
+UPLOAD_FOLDER = './img_dir'  # 이미지를 저장할 위치
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-if not os.path.exists(app.config['UPLOAD_FOLDER']): #디렉토리 없는 경우 자동 생성
-            os.makedirs(app.config['UPLOAD_FOLDER'])
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+os.makedirs('./static', exist_ok=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -18,15 +20,14 @@ def index():
             gpt4_instance = Gpt4(key = key)
             
         image = request.files['image']  # 이미지 받기
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)  # 이미지를 저장할 경로
+        filepath = f"{app.config['UPLOAD_FOLDER']}/{image.filename}"
         image.save(filepath)  # 이미지 저장
+        shutil.copy(filepath, f'./static/{image.filename}')     # 이미지를 static 폴더에 복사 -> static 폴더에 있어야 html에서 출력가능
 
         sentence = request.form['sentence']  # 문장 받기
-        # result_image, result_sentence = gpt4_instance.parse(filepath, sentence)  # 이미지 파일 경로와 문장을 gpt4로 전달
         result_sentence = gpt4_instance.parse(filepath, sentence)  # 현재 문장만 생성하기 때문에 출력값 하나로 축소
 
-        # return render_template('result.html', image=result_image, sentence=result_sentence)  # 결과 표시
-        return render_template('result.html', sentence=result_sentence)  # 현재 문장만 생성하기 때문에 출력값 하나로 축소
+        return render_template('result.html', image=image.filename, sentence=result_sentence)  # 결과 표시
     
     return render_template('index.html')  # 초기 페이지 표시
 
